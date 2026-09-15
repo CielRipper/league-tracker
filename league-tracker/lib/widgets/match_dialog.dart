1aa75fde2d72e85dec899b9a9a3c9b4a4c1569c3
CielRipper <jaxontreadwell@gmail.com>
2026-09-15 14:11:57 -0500

    added match dialog state fields and a date picker

---
+++ league-tracker/lib/widgets/match_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:to_dont_list/objects/match_record.dart';
+
+typedef MatchListAddedCallback = Function(MatchRecord record);
+
+class MatchDialog extends StatefulWidget {
+  const MatchDialog({
+    super.key,
+    required this.onMatchAdded,
+  });
+
+  final MatchListAddedCallback onMatchAdded;
+
+  @override
+  State<MatchDialog> createState() => _MatchDialogState();
+}
+
+class _MatchDialogState extends State<MatchDialog> {
+  final TextEditingController _championController = TextEditingController();
+  Role _selectedRole = Role.top;
+  final TextEditingController _killsController = TextEditingController();
+  final TextEditingController _deathsController = TextEditingController();
+  final TextEditingController _assistsController = TextEditingController();
+  final TextEditingController _csController = TextEditingController();
+  final TextEditingController _gameLengthController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  bool _won = true;
+}
+
+
